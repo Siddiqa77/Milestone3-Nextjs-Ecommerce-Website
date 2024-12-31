859b2dd001ce1a33ef3402f80a0c2ea24bccc4c3
Siddiqa Badar <siddiqaasim778899@gmail.com>
2025-01-01 02:39:24 +0500

    update new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,4 @@
-// import Bottombanner from '@/components/Bottombanner'
-// import Hero from '@/components/Hero'
-// import React from 'react'
-// import ProductList from '../components/ProductList';
 
-// const Home = () => {
-//   return (
-//     <div>
-//      <Hero/>
-    
-//      <ProductList/>
-//      <Bottombanner/>
-//     </div>
-//   )
-// }
-
-// export default Home
 "use client";
 
 
